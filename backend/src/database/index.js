b618f9943813ea9f64a4955a849f0adbca4e1d92
Otávio Pace <otaviopp8@gmail.com>
2020-07-21 18:42:16 -0300

    index: ensure database is connected before starting server

--- backend/src/database/index.js
+++ backend/src/database/index.js
@@ -1,6 +1,30 @@
+const Promise = require('bluebird')
 const Sequelize = require('sequelize')
 const config = require('../config/database')
+const DatabaseError = require('../errors/database')
 
 const sequelize = new Sequelize(config)
 
-module.exports = sequelize
+const ensureDbIsConnected = (db) => {
+  const MAX_RETRIES = 10
+  const RETRY_TIMEOUT = 1000
+
+  const tryToConnect = (retry = 1) =>
+    db.authenticate()
+      .catch((err) => {
+        console.log('tried:', retry)
+        if (retry < MAX_RETRIES) {
+          return Promise.delay(RETRY_TIMEOUT)
+            .then(() => tryToConnect(retry + 1))
+        }
+
+        return Promise.reject(new DatabaseError(err))
+      })
+
+  return Promise.resolve(tryToConnect())
+}
+
+module.exports = {
+  sequelize,
+  ensureDbIsConnected,
+}
--- backend/src/index.js
+++ backend/src/index.js
@@ -2,7 +2,8 @@ const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const setupDotenv = require('./config')
-const database = require('./database')
+const { sequelize, ensureDbIsConnected } = require('./database')
+const DatabaseError = require('./errors/database')
 
 setupDotenv()
 
@@ -14,8 +15,12 @@ app.use(bodyParser.json())
 app.get('/', (req, res) => res.sendStatus(200))
 app.post('/', (req, res) => res.status(200).send(req.body))
 
-database.authenticate()
-  .then(() => console.log('success on database connection'))
-  .catch(error => console.error('failed to connect to database, error:', error))
+const startServer = application =>
+  application.listen(process.env.PORT || 3000)
 
-app.listen(process.env.PORT || 3000)
+ensureDbIsConnected(sequelize)
+  .then(() => console.log('success on database connection'))
+  .then(() => startServer(app))
+  .then(() => console.log('server started listening'))
+  .catch(DatabaseError, error => console.error('failed to connect to database, error:', error))
+  .catch(error => console.error('unknown error:', error))
