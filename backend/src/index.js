c0413621cd783283b77fb626d0a1a77812680ad3
Otávio Pace <otaviopp8@gmail.com>
2020-07-22 15:29:07 -0300

    server: finish authentication with sessions and auth middleware

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,9 @@
 const Promise = require('bluebird')
-const passport = require('passport')
 const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 const setupDotenv = require('./config')
 const setupGracefulShutdown = require('./shutdown')
 const { connectToDatabase } = require('./database')
@@ -15,9 +15,32 @@ const app = express()
 
 app.use(cors())
 app.use(bodyParser.json())
+const authMiddleware = (req, res, next) => {
+  const authHeader = req.get('Authorization')
 
-app.get('/', (req, res) => res.sendStatus(200))
-app.post('/', (req, res) => res.status(200).send(req.body))
+  if (!authHeader) {
+    return res.status(401).send({ errors: [{ message: 'authorization header is missing' }] })
+  }
+
+  const [bearer, token] = authHeader.split(' ')
+
+  if (bearer !== 'Bearer') {
+    return res.status(401).send({ errors: [{ message: 'the string `Bearer` should prefix the auth token' }] })
+  }
+
+  try {
+    const decodedUser = jwt.verify(token, process.env.JWT_SECRET)
+
+    req.user = decodedUser
+
+    return next()
+  } catch (error) {
+    return res.status(401).send({ errors: [{ message: 'invalid authorization token' }] })
+  }
+}
+
+app.get('/', authMiddleware, (req, res) => res.sendStatus(200))
+app.post('/', authMiddleware, (req, res) => res.status(200).send(req.body))
 
 const startServer = application =>
   application.listen(process.env.PORT || 3000)
@@ -46,6 +69,39 @@ const start = () => Promise.resolve((async () => {
     delete user.dataValues.password_hash
 
     return res.status(201).send(user)
+  })
+
+  app.post('/sessions', async (req, res) => {
+    const username = req.body.username
+    const password = req.body.password
+    const user = await db.models.User.findOne({ where: {username} })
+
+    if (!user) {
+      return res.status(401).send({ errors: [{ message: 'username or passord are incorrect !user' }] })
+    }
+
+    const isCorrectPassword = await bcrypt.compare(password, user.password_hash)
+
+    if (!isCorrectPassword) {
+      return res.status(401).send({ errors: [{ message: 'username or passord are incorrect !isCorrectPassword' }] })
+    }
+
+    const jwtPayload = {
+      id: user.id,
+      username,
+    }
+
+    const expiresIn = 604800 // 1 week in seconds
+
+    const token = jwt.sign(
+      jwtPayload,
+      process.env.JWT_SECRET,
+      { expiresIn }
+    )
+
+    return res.status(201).send({
+      token,
+    })
   })
 
   const server = startServer(app)
