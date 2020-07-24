82f6271686fb02b5c793d0111c19ecefa1c91436
Otávio Pace <otaviopp8@gmail.com>
2020-07-24 13:53:13 -0300

    index: refactor error handling

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,3 @@
-const Promise = require('bluebird')
 const { createServer, startServer } = require('./ports/http-server')
 const { listenWebSocket } = require('./ports/web-socket')
 const setupDotenv = require('./config')
@@ -7,18 +6,25 @@ const DatabaseError = require('./errors/database')
 
 setupDotenv()
 
-const start = () => Promise.resolve((async () => {
-  const db = await connectToDatabase()
-  console.log('success on database connection')
+const start = async () => {
+  try {
+    const db = await connectToDatabase()
+    console.log('success on database connection')
 
-  const server = createServer(db)
+    const server = createServer(db)
 
-  listenWebSocket(server, db)
+    listenWebSocket(server, db)
 
-  startServer(server)
-  console.log('server started listening')
-})())
+    startServer(server)
+    console.log('server started listening')
+  } catch (error) {
+    if (error instanceof DatabaseError) {
+      console.error('failed to connect to database, error:', error)
+      return
+    }
+
+    console.error('unknown error:', error)
+  }
+}
 
 start()
-  .catch(DatabaseError, error => console.error('failed to connect to database, error:', error))
-  .catch(error => console.error('unknown error:', error))
