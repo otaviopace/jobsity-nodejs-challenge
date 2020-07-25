1e14d9a808c3a9d90fc162f83f56bf7de5ffef76
Otávio Pace <otaviopp8@gmail.com>
2020-07-25 11:47:57 -0300

    repository: add tests for sequelize conversion

--- backend/tests/mocks/index.js
+++ backend/tests/mocks/index.js
@@ -2,6 +2,15 @@ const createFakeIO = () => ({
   emit: jest.fn(),
 })
 
+const createFakeSequelizeModel = () => ({
+  create: jest.fn().mockReturnValueOnce(Promise.resolve()),
+  findOne: jest.fn().mockReturnValueOnce(Promise.resolve()),
+  findAll: jest.fn().mockReturnValueOnce(Promise.resolve()),
+  update: jest.fn().mockReturnValueOnce(Promise.resolve()),
+  destroy: jest.fn().mockReturnValueOnce(Promise.resolve()),
+})
+
 module.exports = {
   createFakeIO,
+  createFakeSequelizeModel,
 }
